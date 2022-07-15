28f4baf9eb75921cd0c85175db1f3b2f3731a828
Andrew Pinson <andrew.pinson@q4inc.com>
2022-07-15 09:20:56 -0700

    Fixing cleanup if version is greater than 10.

--- deleteTagsByBranchName.sh
+++ deleteTagsByBranchName.sh
@@ -4,7 +4,7 @@ tag_format=${git_branch//[^a-zA-Z0-9]/-}
 echo $tag_format
 git fetch --all --tags
 #search patten vX.X.X-branchName-x
-declare -a tags=( $( git tag --list "v[0-9].[0-9].[0-9]-${tag_format}.[0-9]*") )
+declare -a tags=( $( git tag --list "v[0-9]*.[0-9]*.[0-9]*-${tag_format}.[0-9]*") )
 for tag_name in "${tags[@]}"
 do
   node index.js ${tag_name}
